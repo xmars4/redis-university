c333e5e23291ab5bc5c32dfc6a89d4849e14213e
xmars <xtruongptit@gmail.com>
2023-02-24 09:19:26 +0700

    init

--- redisolar/dao/redis/site.py
+++ redisolar/dao/redis/site.py
@@ -37,8 +37,14 @@ class SiteDaoRedis(SiteDaoBase, RedisDaoBase):
     def find_all(self, **kwargs) -> Set[Site]:
         """Find all Sites in Redis."""
         # START Challenge #1
-        # Remove this line when you've written code to build `site_hashes`.
-        site_hashes = []  # type: ignore
-        # END Challenge #1
+        key_schema = self.key_schema
+        site_ids_key = key_schema.site_ids_key()
+        site_ids = self.redis.smembers(site_ids_key)
+
+        site_hashes = []
+        for site_id in site_ids:
+            site_hash_key = key_schema.site_hash_key(site_id)
+            site_hash = self.redis.hgetall(site_hash_key)
+            site_hashes.append(site_hash)
 
         return {FlatSiteSchema().load(site_hash) for site_hash in site_hashes}
